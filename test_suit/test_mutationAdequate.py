5bcdee8b7461df86fcbf74d11afa8636a7c30473
Varun <v1a9r9u8n@gmail.com>
2023-10-23 21:59:28 -0400

    minor: updated class names

--- test_suit/test_mutationAdequate.py
+++ test_suit/test_mutationAdequate.py
@@ -4,7 +4,7 @@ from isTriangle import Triangle
 # sys.path.append("..")
 # from src.Triangle import Triangle
 
-class TriangleTest(unittest.TestCase):
+class MutationAdequateTest(unittest.TestCase):
 
     def test1(self):
         actual = Triangle.classify(10, 10, 10)
